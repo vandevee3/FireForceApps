5561f68760ba55eaade0cc9c7b8c156a57bc192b
vandevee3 <garindrawardhana@gmail.com>
2021-11-21 15:34:21 +0700

    commmeeet

--- app/src/main/java/com/example/fireforceapps/BuildingCondition.java
+++ app/src/main/java/com/example/fireforceapps/BuildingCondition.java
@@ -10,5 +10,8 @@ public class BuildingCondition extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building_condition);
+
+        getSupportActionBar().hide();
+        
     }
 }
