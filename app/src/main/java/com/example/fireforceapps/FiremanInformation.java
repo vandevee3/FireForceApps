1397218bedacb278e37cf8765ba14759dd780caa
vandevee3 <garindrawardhana@gmail.com>
2021-11-21 22:31:23 +0700

    Furqon BABI

--- app/src/main/java/com/example/fireforceapps/FiremanInformation.java
+++ app/src/main/java/com/example/fireforceapps/FiremanInformation.java
@@ -4,14 +4,41 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
-public class FiremanInformation extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class FiremanInformation extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap mapAPI;
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fireman_information);
 
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getColor(R.color.red)));
+
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapAPI);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapAPI = googleMap;
+
+        LatLng PemadamKebakaranSurabaya = new LatLng(-7.322177758112991, 112.71372000897117);
+
+        mapAPI.addMarker(new MarkerOptions().position(PemadamKebakaranSurabaya).title("Damkar"));
+
+        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(PemadamKebakaranSurabaya));
     }
 }
