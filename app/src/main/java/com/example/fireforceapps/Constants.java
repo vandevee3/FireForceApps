ef7cd5ffcf82adaa680069fe1f30629405bc75d6
rafidzia <32030497+rafidzia@users.noreply.github.com>
2021-11-25 00:54:54 +0700

    change socket.io url

--- app/src/main/java/com/example/fireforceapps/Constants.java
+++ app/src/main/java/com/example/fireforceapps/Constants.java
@@ -1,5 +1,5 @@
 package com.example.fireforceapps;
 
 public class Constants {
-    public static final String SOCKET_SERVER_URL = "http://hompimpa.herokuapp.com/";
+    public static final String SOCKET_SERVER_URL = "http://103.126.226.28:8001/";
 }
